a5efae022b8c00c32495b5191da2fdbcd54d04e8
SmaylovSerikbay <alibekov.madi@gmail.com>
2024-07-17 15:19:44 +0500

    Update World.js

--- src/Experience/World/World.js
+++ src/Experience/World/World.js
@@ -1,8 +1,8 @@
 import * as THREE from "three";
 import Experience from "@Experience/Experience.js";
+
 import Environment from "@World/Environment.js";
-import Box from "@World/Box.js"; // Ваш существующий класс Box
-import Map from "@World/Map.js"; // Класс Map, который вы добавили
+import Box from "@World/Box.js";
 
 export default class World {
   constructor() {
@@ -11,25 +11,14 @@ export default class World {
     this.resources = this.experience.resources;
 
     this.resources.on("loaded", () => {
-      this.environment = new Environment();
+      this.enviorment = new Environment();
       this.box = new Box();
-      this.map = new Map(); // Добавляем карту
-      this.createCube(); // Создаем куб
     });
   }
 
-  createCube() {
-    // Создание геометрии и материала для куба
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
-    cube.position.set(0, 0, 0); // Установка позиции куба
-    this.scene.add(cube);
-  }
-
   update() {
-    if (this.box) {
-      this.box.update();
+    if (this.fox) {
+      this.fox.update();
     }
   }
 }
